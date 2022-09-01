3b1aeb8851a0b9c8c0a9eebbd295cbb5cf24aa36
Gabriel Rocha <gabrielrochaas@gmail.com>
2022-08-31 21:20:54 -0300

    chore: add Roboto font

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,29 +1,67 @@
 import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
-html {
-  box-sizing: border-box;
-  background: #f5f4f0;
-  display: block;
-  height: 100%;
-  max-height: 640px;
-  margin: 0 auto;
+* {
+  margin: 0;
   padding: 0;
+  box-sizing: border-box;
+}
+
+@media (min-width: 1024px) {
+  html {
+    font-size: 93.75%;
+  }
+}
+
+@media (min-width:768px) {
+  html {
+    font-size: 87.5%;
+  }
 }
 
 body {
   background-color: #fafafa;
-  min-height: 100vh;
-  padding: 0;
-  margin: 0;
-  font-family: Verdana, Geneva, Tahoma, sans-serif;
+  margin: 0 auto;
+
+  color: #353535;
+}
+
+p,
+h4,
+h5,
+h6,
+body,
+button,
+input,
+select,
+textarea {
+  font: 400 1rem 'Roboto', 'Helvetica Neue', sans-serif;
+}
+
+h1,
+h2,
+h3 {
+  font: 600 2rem 'Roboto', 'Helvetica Neue', sans-serif;
+}
+
+a {
+  color: #0f6643;
+  text-decoration: none;
+
+  &:hover {
+    color: #23a455;
+  }
+}
+
+button {
+  cursor: pointer;
 }
 `;
 
 export const theme = {
   colors: {
-    primary: '#fafafa',
+    primary: '#353535',
+    link: '#0f6643',
+    linkHover: '#23a455',
   },
 };
-
-
